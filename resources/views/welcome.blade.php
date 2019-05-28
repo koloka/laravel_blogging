f4e3215de41797857b7c66734eeaade95b56578c
koloka <46620661+koloka@users.noreply.github.com>
2019-05-28 11:31:46 +0700

    document

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel 000000000000000000000
                 </div>
 
                 <div class="links">
